f8792f78da9f18ba168bf2244e4054c8eb37ecae
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-11 09:53:21 -0500

    se completa employee

--- src/main/java/com/linadev14/refactor_management_mongodb/domain/document/Employee.java
+++ src/main/java/com/linadev14/refactor_management_mongodb/domain/document/Employee.java
@@ -1,3 +1,21 @@
 package com.linadev14.refactor_management_mongodb.domain.document;
+
+import com.linadev14.refactor_management_mongodb.domain.valueObject.Country;
+import com.linadev14.refactor_management_mongodb.domain.valueObject.Gender;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(value = "employee")
 public class Employee {
+
+    @Id
+    private final String id;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String phoneNumber;
+    private final Country country;
+    private final Gender gender;
 }

b4fc23c72c8b021b7ddbbb5035d657b996224c1a
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-11 10:14:06 -0500

    se crea mapper mapToEmployeeDTO

--- src/main/java/com/linadev14/refactor_management_mongodb/application/mapper/EmployeeMapper.java
+++ src/main/java/com/linadev14/refactor_management_mongodb/application/mapper/EmployeeMapper.java
@@ -1,3 +1,25 @@
 package com.linadev14.refactor_management_mongodb.application.mapper;
+
+import com.linadev14.refactor_management_mongodb.domain.document.Employee;
+import com.linadev14.refactor_management_mongodb.domain.dto.EmployeeDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
 public class EmployeeMapper {
+
+    // FROM EMPLOYEE TO EMPLOYEE_DTO
+    public Function<Employee, EmployeeDTO> mapToEmployeeDTO(){
+
+        return employee -> new EmployeeDTO(
+          employee.getId(),
+          employee.getFirstName(),
+          employee.getLastName(),
+          employee.getEmail(),
+          employee.getPhoneNumber(),
+          employee.getCountry(),
+          employee.getGender()
+        );
+    }
 }

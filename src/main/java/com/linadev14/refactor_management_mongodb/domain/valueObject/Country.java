60e0a2fdba4b2eb84cffb1008618565ad6f8b6e2
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-11 10:03:16 -0500

    se crea employeeMapper

--- src/main/java/com/linadev14/refactor_management_mongodb/domain/valueObject/Country.java
+++ src/main/java/com/linadev14/refactor_management_mongodb/domain/valueObject/Country.java
@@ -1,3 +1,11 @@
 package com.linadev14.refactor_management_mongodb.domain.valueObject;
 public enum Country {
+
+    COLOMBIA,
+    PERU,
+    VENEZUELA,
+    BRAZIL,
+    BOLIVIA,
+    ECUADOR,
+    CHILE
 }
--- src/main/java/com/linadev14/refactor_management_mongodb/domain/valueObject/Gender.java
+++ src/main/java/com/linadev14/refactor_management_mongodb/domain/valueObject/Gender.java
@@ -1,3 +1,6 @@
 package com.linadev14.refactor_management_mongodb.domain.valueObject;
 public enum Gender {
+
+    MALE,
+    FEMALE
 }

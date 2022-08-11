1b6f1bd65c4f734cb71033f7b486b37f2320b633
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-11 09:55:23 -0500

    se completa employeeDTO

--- src/main/java/com/linadev14/refactor_management_mongodb/domain/dto/EmployeeDTO.java
+++ src/main/java/com/linadev14/refactor_management_mongodb/domain/dto/EmployeeDTO.java
@@ -1,3 +1,21 @@
 package com.linadev14.refactor_management_mongodb.domain.dto;
+
+import com.linadev14.refactor_management_mongodb.domain.valueObject.Country;
+import com.linadev14.refactor_management_mongodb.domain.valueObject.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDTO {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private Country country;
+    private Gender gender;
 }

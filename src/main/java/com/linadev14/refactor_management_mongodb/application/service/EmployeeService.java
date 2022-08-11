f605afbdc69ce4f05e490d3f2336199fb49714fa
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-11 10:31:10 -0500

    se crean metodos en interface

--- src/main/java/com/linadev14/refactor_management_mongodb/application/service/EmployeeService.java
+++ src/main/java/com/linadev14/refactor_management_mongodb/application/service/EmployeeService.java
@@ -1,6 +1,71 @@
 package com.linadev14.refactor_management_mongodb.application.service;
+import com.linadev14.refactor_management_mongodb.application.mapper.EmployeeMapper;
+import com.linadev14.refactor_management_mongodb.domain.dto.EmployeeDTO;
+import com.linadev14.refactor_management_mongodb.domain.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class EmployeeService implements EmployeeServiceInterface{
+
+    private final EmployeeRepository employeeRepository;
+    private final EmployeeMapper employeeMapper;
+
+
+    @Override
+    public List<EmployeeDTO> fillData(List<EmployeeDTO> employeeDTO) {
+
+        return null;
+    }
+
+    @Override
+    public List<EmployeeDTO> listEmployee() {
+
+        return null;
+    }
+
+    @Override
+    public Optional<EmployeeDTO> findById(String id) {
+
+        return Optional.empty();
+    }
+
+    @Override
+    public List<EmployeeDTO> findAllByFirstNameContainingIgnoreCaseOrderByFirstName(String firstName) {
+
+        return null;
+    }
+
+    @Override
+    public List<EmployeeDTO> findAllByLastNameContainingIgnoreCaseOrderByLastName(String lastName) {
+
+        return null;
+    }
+
+    @Override
+    public EmployeeDTO updateEmployee(EmployeeDTO employeeDTO) {
+
+        return null;
+    }
+
+    @Override
+    public EmployeeDTO saveEmployee(EmployeeDTO employeeDTO) {
+
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(String id) {
+
+    }
+
+    @Override
+    public void deleteAllEmployees() {
+
+    }
+
 }
--- src/main/java/com/linadev14/refactor_management_mongodb/application/service/EmployeeServiceInterface.java
+++ src/main/java/com/linadev14/refactor_management_mongodb/application/service/EmployeeServiceInterface.java
@@ -1,3 +1,34 @@
 package com.linadev14.refactor_management_mongodb.application.service;
+import com.linadev14.refactor_management_mongodb.domain.dto.EmployeeDTO;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface EmployeeServiceInterface {
+
+    // save all data
+    List<EmployeeDTO> fillData(List<EmployeeDTO> employeeDTO);
+
+    //get employees
+    List<EmployeeDTO> listEmployee();
+
+    // get employees by id
+    Optional<EmployeeDTO> findById(String id);
+
+    // find employees by firstname
+    List<EmployeeDTO> findAllByFirstNameContainingIgnoreCaseOrderByFirstName(String firstName);
+
+    // find employees by last name
+    List<EmployeeDTO> findAllByLastNameContainingIgnoreCaseOrderByLastName(String lastName);
+
+    // update employee
+    EmployeeDTO updateEmployee(EmployeeDTO employeeDTO);
+    // save a new employee
+    EmployeeDTO saveEmployee(EmployeeDTO employeeDTO);
+
+    // delete employee by id
+    void deleteEmployee(String id);
+
+    // delete all employees
+    void deleteAllEmployees();
 }

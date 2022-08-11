44d5cd1030bbefd414bff31fed50d1df0d5ec7ec
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-11 09:34:09 -0500

    se crea conexion con mongodb

---
+++ src/main/java/com/linadev14/refactor_management_mongodb/RefactorManagementMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.linadev14.refactor_management_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorManagementMongodbApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RefactorManagementMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linadev14/refactor_management_mongodb/RefactorManagementMongodbApplicationTests.java
@@ -0,0 +1,14 @@
+package com.linadev14.refactor_management_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactorManagementMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}

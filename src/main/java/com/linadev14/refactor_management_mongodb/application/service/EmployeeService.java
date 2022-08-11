700f37dfa428b09ee709054eab839c3368a522b7
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-11 10:06:54 -0500

    se crea employeeService

--- src/main/java/com/linadev14/refactor_management_mongodb/application/service/EmployeeService.java
+++ src/main/java/com/linadev14/refactor_management_mongodb/application/service/EmployeeService.java
@@ -1,3 +1,6 @@
 package com.linadev14.refactor_management_mongodb.application.service;
-public class EmployeeService {
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService implements EmployeeServiceInterface{
 }

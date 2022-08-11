f573176ba88358d99bf702ca5ae4cc19d4beed06
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-11 09:57:42 -0500

    se completa EmployeeRepository

--- src/main/java/com/linadev14/refactor_management_mongodb/domain/repository/EmployeeRepository.java
+++ src/main/java/com/linadev14/refactor_management_mongodb/domain/repository/EmployeeRepository.java
@@ -1,3 +1,9 @@
 package com.linadev14.refactor_management_mongodb.domain.repository;
-public interface EmployeeRepository {
+
+import com.linadev14.refactor_management_mongodb.domain.document.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
 }

2f9afbba3f75805450ac6034de6083a2fcdc3e2e
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-12 11:37:40 -0500

    se crea employeeCommandController

---
+++ src/main/java/com/linadev14/refactor_management_mongodb/infraestructure/commads/EmployeeCommandController.java
@@ -0,0 +1,20 @@
+package com.linadev14.refactor_management_mongodb.infraestructure.commads;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/command/employee")
+@RequiredArgsConstructor
+public class EmployeeCommandController {
+
+    /*
+     * GET -> consult
+     * POST -> create
+     * PUT -> Edit
+     * DELETE -> delete
+     * */
+
+
+}
